38c1cf2d00ba758ee19fe0d919e5845e60e02f39
Richard Jiang <Richard Jiang>
2017-04-20 15:15:52 -0700

    Completed Exercise

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,33 +1,48 @@
 ### Exercise 1 ###
 
 # Load the httr and jsonlite libraries for accessing data
-
+install.packages("httr")
+library(httr)
+library(jsonlite)
 
 ## For these questions, look at the API documentation to identify the appropriate endpoint and information.
 ## Then send GET() request to fetch the data, then extract the answer to the question
 
 # For what years does the API have statistical data?
-
+response <- GET("http://data.unhcr.org/api/stats/time_series_years.json")
+body <- content(response, "text")
+year <- fromJSON(body)
 
 # What is the "country code" for the "Syrian Arab Republic"?
-
+response <- GET("http://data.unhcr.org/api/countries/list.json")
+body <- content(response, 'text')
+countries <- fromJSON(body)
+countries$country_code[countries$name_en == "Syrian Arab Republic"]
 
 # How many persons of concern from Syria applied for residence in the USA in 2013?
 # Hint: you'll need to use a query parameter
 # Use the `str()` function to print the data of interest
 # See http://www.unhcr.org/en-us/who-we-help.html for details on these terms
-
+query.parameters <- list("country_of_residence" = "USA", "country_of_origin" = "SYR", "year" = 2013)
+response <- GET("http://data.unhcr.org/api/stats/persons_of_concern.json", query = query.parameters)
+body <- content(response, "text")
+from.syria <- fromJSON(body)
+from.syria$refugees
 
 ## And this was only 2013...
 
 
 # How many *refugees* from Syria settled the USA in all years in the data set (2000 through 2013)?
 # Hint: check out the "time series" end points
-
+query.parameters <- list("country_of_residence" = "USA", "country_of_origin" = "SYR", "population_type_code" = "RF")
+response <- GET("http://data.unhcr.org/api/stats/time_series_all_years.json", query = query.parameters)
+body <- content(response, "text")
+years.syria <- fromJSON(body)
+years.syria <- select(years.syria, year, value)
 
 # Use the `plot()` function to plot the year vs. the value.
 # Add `type="o"` as a parameter to draw a line
-
+plot(years.syria, type = "o")
 
 
 # Pick one other country in the world (e.g., Turkey).
